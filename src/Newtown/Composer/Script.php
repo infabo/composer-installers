1cf061b7b014125c3c32b22990cbf4bb57dc4106
Ingo Fabbri <if@newtown.at>
2016-10-12 10:28:37 +0200

    fix for wrong used event type

--- src/Newtown/Composer/Script.php
+++ src/Newtown/Composer/Script.php
@@ -2,6 +2,9 @@
 
 namespace Newtown\Composer;
 
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\UninstallOperation;
+use Composer\DependencyResolver\Operation\UpdateOperation;
 use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
 use Composer\Util\ProcessExecutor;
@@ -19,18 +22,38 @@ class Script
 
     public static function postPackageInstall(PackageEvent $event)
     {
-        $installedPackage = $event->getOperation()->getPackage();
+        /** @var InstallOperation $operation */
+        $operation = $event->getOperation();
+        $installedPackage = $operation->getPackage();
 
-        $processExecutor = new ProcessExecutor($event->getIO());
-        $processExecutor->execute(sprintf('modman deploy %s', $installedPackage));
+        if ($installedPackage->getType() === 'magento-module') {
+            $processExecutor = new ProcessExecutor($event->getIO());
+            $processExecutor->execute(sprintf('modman deploy %s', $installedPackage->getName()));
+        }
     }
 
     public static function postPackageUpdate(PackageEvent $event)
     {
-        $installedPackage = $event->getOperation()->getPackage();
+        /** @var UpdateOperation $operation */
+        $operation = $event->getOperation();
+        $installedPackage = $operation->getTargetPackage();
 
-        $processExecutor = new ProcessExecutor($event->getIO());
-        $processExecutor->execute(sprintf('modman deploy %s', $installedPackage));
+        if ($installedPackage->getType() === 'magento-module') {
+            $processExecutor = new ProcessExecutor($event->getIO());
+            $processExecutor->execute(sprintf('modman deploy %s', $installedPackage->getName()));
+        }
+    }
+
+    public static function prePackageUninstall(PackageEvent $event)
+    {
+        /** @var UninstallOperation $operation */
+        $operation = $event->getOperation();
+        $installedPackage = $operation->getPackage();
+
+        if ($installedPackage->getType() === 'magento-module') {
+            $processExecutor = new ProcessExecutor($event->getIO());
+            $processExecutor->execute(sprintf('modman undeploy %s', $installedPackage->getName()));
+        }
     }
 
     public static function warmCache(Event $event)
