d693ab61a3876c812516c4421a1de06dd2409277
Ingo Fabbri <if@newtown.at>
2017-06-27 11:04:02 +0200

    add some events automation

--- src/Aoepeople/ComposerInstallers/InstallerPlugin.php
+++ src/Aoepeople/ComposerInstallers/InstallerPlugin.php
@@ -12,14 +12,101 @@ namespace Aoepeople\ComposerInstallers;
  */
 
 use Composer\Composer;
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\UpdateOperation;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvent;
 use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Util\ProcessExecutor;
 
-class InstallerPlugin implements PluginInterface
+class InstallerPlugin implements PluginInterface, EventSubscriberInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'pre-update-cmd' => array(
+                array('magentoEnableMaintenance'),
+            ),
+            'post-autoload-dump' => array(
+                array('magentoFlushCache'),
+                array('magentoRunSetup'),
+                array('magentoDisableMaintenance'),
+            ),
+            'post-package-install' => array(
+                array('postPackageInstall')
+            ),
+            'post-package-update' => array(
+                array('postPackageUpdate')
+            ),
+            'pre-package-uninstall' => array(
+                array('modmanUndeployPackage')
+            )
+        );
+    }
+
+    public static function postPackageInstall(PackageEvent $event)
+    {
+        $binDir = $event->getComposer()->getConfig()->get('bin-dir');
+
+        /** @var InstallOperation $operation */
+        $operation = $event->getOperation();
+        $installedPackage = $operation->getPackage();
+
+        if ($installedPackage->getType() === 'magento-module') {
+            $processExecutor = new ProcessExecutor($event->getIO());
+            $processExecutor->execute(sprintf('%s/modman deploy %s', $binDir, static::getModmanName($installedPackage)));
+        }
+    }
+
+    public static function postPackageUpdate(PackageEvent $event)
+    {
+        $binDir = $event->getComposer()->getConfig()->get('bin-dir');
+
+        /** @var UpdateOperation $operation */
+        $operation = $event->getOperation();
+        $updatedPackage = $operation->getTargetPackage();
+
+        if ($updatedPackage->getType() === 'magento-module') {
+            $processExecutor = new ProcessExecutor($event->getIO());
+            $processExecutor->execute(sprintf('%s/modman deploy %s', $binDir, static::getModmanName($updatedPackage)));
+        }
+    }
+
+    public function magentoEnableMaintenance(Event $event)
+    {
+        $binDir = $event->getComposer()->getConfig()->get('bin-dir');
+
+        $processExecutor = new ProcessExecutor($event->getIO());
+        $processExecutor->execute($binDir . '/n98-magerun sys:maintenance --on');
+    }
+
+    public function magentoDisableMaintenance(Event $event)
+    {
+        $binDir = $event->getComposer()->getConfig()->get('bin-dir');
+
+        $processExecutor = new ProcessExecutor($event->getIO());
+        $processExecutor->execute($binDir . '/n98-magerun sys:maintenance --off');
+    }
+
+    protected static function getModmanName(PackageInterface $package)
+    {
+        $prettyName = $package->getPrettyName();
+        if (strpos($prettyName, '/') !== false) {
+            list($vendor, $name) = explode('/', $prettyName);
+        } else {
+            $vendor = '';
+            $name = $prettyName;
+        }
+
+        return "{$vendor}_{$name}";
     }
 }
