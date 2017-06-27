d4df20449173d0d57d0c748ead953d8260548336
Ingo Fabbri <if@newtown.at>
2017-06-27 08:44:38 +0200

    install core-package only if defined via composer config

--- src/Aoepeople/ComposerInstallers/MagentoInstaller.php
+++ src/Aoepeople/ComposerInstallers/MagentoInstaller.php
@@ -1,10 +1,28 @@
 <?php
+
 namespace Aoepeople\ComposerInstallers;
 
 class MagentoInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'module'   => '.modman/{$vendor}_{$name}/',
-        'source'    => 'htdocs/'
+        'module' => '.modman/{$vendor}_{$name}/'
     );
+
+    /**
+     * @param array $packageExtra
+     */
+    public function __construct(array $packageExtra)
+    {
+        parent::__construct();
+
+        if (isset($packageExtra['magento-core-package-type'])) {
+            //merge excludes from root package composer.json file with default excludes
+            if (!isset($packageExtra['magento-root-dir'])) {
+                throw new \InvalidArgumentException("magento-root-dir must be specified in root package");
+            }
+
+            $this->locations[$packageExtra['magento-core-package-type']] = rtrim($packageExtra['magento-root-dir'],
+                '/');
+        }
+    }
 }
